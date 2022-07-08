b209b93116039ced4f39bee249624ea67fe95376
dinhquocnhat <nhatd87@gmail.com>
2022-07-08 14:50:54 +0700

    add file checlk prime

--- ExamsFromMrCu/src/Total.java
+++ ExamsFromMrCu/src/Total.java
@@ -9,9 +9,17 @@ public class Total {
         int y = sc.nextInt();
         int sumOfSquares = 0;
         // i chay tu x den y, nen gan i = x
+        if (y == 0) {
+            System.out.println("Sai");
+            return;
+        }
+        if (x > y) {
+            System.out.println("Sai");
+            return;
+        }
         for (int i = x; i <= y; i++) {
             sumOfSquares += Math.pow(i, 2);
         }
-        System.out.printf("sum of squares form %d to %d: %d",x,y,sumOfSquares);
+        System.out.printf("sum of squares form %d to %d: %d", x, y, sumOfSquares);
     }
 }
