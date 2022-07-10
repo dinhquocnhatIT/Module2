cdadb583cecefd2d190ba0357edfba21dae2cf72
dinhquocnhat <nhatd87@gmail.com>
2022-07-10 09:22:12 +0700

    add file check prime changs

--- LoopJava/src/CheckPrime.java
+++ LoopJava/src/CheckPrime.java
@@ -5,25 +5,26 @@ public class CheckPrime {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter number to check prime");
         int n = scanner.nextInt();
-        boolean isPrime = true;
+
         if (n < 2) {
             System.out.println("It's not a prime");
         }
         else {
             int i = 2;
+            boolean isPrime = true;
             while (i <= Math.sqrt(n)) {
                 if (n % i == 0) {
                     isPrime = false;
-                    i++;
                     break;
                 }
-                isPrime = true;
+                i++;
+            }
+            if (isPrime ) {
+                System.out.println(n + " " +" is a prime number");
+            } else {
+                System.out.println(n + " " +"is not a prime number");
             }
         }
-        if (isPrime) {
-            System.out.println(n + " " +" is a prime number");
-        } else {
-            System.out.println(n + " " +"is not a prime number");
-        }
+
     }
 }
