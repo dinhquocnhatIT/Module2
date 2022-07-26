32bb830b7569f5de753451bf143a578719090c67
dinhquocnhat <nhatd87@gmail.com>
2022-07-26 15:40:19 +0700

    add file animal and interface bai thuc hanh 1

---
+++ AbstractClass/AnimalAndInterface/src/fruit/Fruit.java
@@ -0,0 +1,7 @@
+package fruit;
+
+import interfaceEdible.Edible;
+
+public abstract class Fruit implements Edible {
+
+}
---
+++ AbstractClass/AnimalAndInterface/src/interfaceEdible/Edible.java
@@ -0,0 +1,5 @@
+package interfaceEdible;
+
+public interface Edible {
+    public abstract String howtoEat();
+}
